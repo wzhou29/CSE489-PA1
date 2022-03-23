4c31fddf56501fd264ba666a6973d08ff11163ce
WeichengZhou <wzhou29@buffalo.edu>
2022-03-23 19:31:03 -0400

    update

--- cse489589_assignment1/wzhou29/src/wzhou29_assignment1.c
+++ cse489589_assignment1/wzhou29/src/wzhou29_assignment1.c
@@ -34,10 +34,47 @@
 #include "../include/global.h"
 #include "../include/logger.h"
 
+// construct boolean
+#ifndef __cplusplus
+typedef unsigned char bool;
+static const bool False = 0;
+static const bool True = 1;
+#endif
+
 #define BACKLOG 5 // Amount of request line up waiting for Accept() function
 
 void ClientH(int PortNumber);
 void ServerH(int PortNumber);
+bool check_port(char *port_num);
+void AUTHOR();
+void IP();
+void PORT();
+void LIST();
+void STATISTICS();
+void BLOCKED();
+void LOGIN();
+void REFRESH();
+void SEND();
+void BROADCAST();
+void BLOCK();
+void UNBLOCK();
+void LOGOUT();
+void EXIT();
+void SENDFILE();
+
+struct user 
+{
+	char* hostname;
+	char* ip_addr;
+	int port_num;
+	int num_msg_sent;
+	int num_msg_rcv;
+	// char status;
+	// int fd;
+	bool is_logged_in;
+	bool is_server;
+	// struct message * queued_messages;
+};
 
 /**
  * main function
@@ -58,16 +95,16 @@ int main(int argc, char **argv)
 	/*Start Here*/
 	if(argc == 3)
 	{
-		if(strcmp("s", argv[1]) == 0)
+		if(strcmp("s", argv[1]) == 0 && check_port(argv[2]) == 1)
 		{
 			ServerH(atoi(argv[2]));
 		}
-		else if(strcmp("c", argv[1]) == 0)
+		else if(strcmp("c", argv[1]) == 0 && check_port(argv[2]) == 1)
 		{
 			ClientH(atoi(argv[2]));
 		}
 		else { 
-			perror("./[name of file] [c/s] [port]"); 
+			cse4589_print_and_log("./[name of file] [c/s] [port]"); 
 			exit(-1); 
 		}
 		
@@ -92,24 +129,23 @@ void ServerH(int PortNumber){
 	ServerHost.sin_family = AF_INET; // Address Family: IPv4
 	ServerHost.sin_addr.s_addr = htonl(INADDR_ANY); // Internet Address       
 	ServerHost.sin_port = htons(PortNumber); // Port Number
-	if (bind(ServerSocket,(struct sockaddr_in *)&ServerHost,sizeof(ServerHost)) != 0){ // Bind socket and check if the socket bind or not if not print error message and exit
+	if (bind(ServerSocket,(struct sockaddr *)&ServerHost,sizeof(ServerHost)) != 0){ // Bind socket and check if the socket bind or not if not print error message and exit
 		perror("Error on binding Socket");
 		close(ServerSocket);
 		exit(-1);
 	}
-		cse4589_print_and_log("it work\n");
-	// //set server socket to lsiten
-	// if(listen(ServerSocket,BACKLOG) != 0){ // Listen to port, if can't listen to port number, it will exit
-	// 	perror("Error on listening to Port");
-	// 	close(ServerSocket);
-	// 	exit(-1);
-	// }
+	//set server socket to lsiten
+	if(listen(ServerSocket,BACKLOG) != 0){ // Listen to port, if can't listen to port number, it will exit
+		perror("Error on listening to Port");
+		close(ServerSocket);
+		exit(-1);
+	}
 
-	// //accepting client socket
-	// int ClientSocket;
-	// int socklen = sizeof(struct sockaddr_in);
-	// struct sockaddr_in ClientHost;    //client information
-	// ClientSocket = accept(ServerSocket, (struct sockaddr_in*)&ClientHost, (socklen_t*)&socklen); // accepting client request
+	//accepting client socket
+	int ClientSocket;
+	int socklen = sizeof(struct sockaddr_in);
+	struct sockaddr_in ClientHost;    //client information
+	ClientSocket = accept(ServerSocket, (struct sockaddr*)&ClientHost, (socklen_t*)&socklen); // accepting client request
 
 	// fd_set MasterList, WatchList;
 	// FD_ZERO(&MasterList);
@@ -168,6 +204,7 @@ void ServerH(int PortNumber){
 	// close(ClientSocket);
 	close(ServerSocket);
 }
+
 // client
 void ClientH(int PortNumber){
 	//creating socket
@@ -180,10 +217,158 @@ void ClientH(int PortNumber){
 	bzero(&ClientHost,sizeof(ClientHost));
 	ClientHost.sin_family = AF_INET;
 	ClientHost.sin_port = htons(PortNumber);
-	cse4589_print_and_log("it work c\n");
 	// if(connect(ClientSocket, (struct sockaddr *)&ClientHost, sizeof(ClientHost)) != 0) {
 	// 	perror("Connecting Error");
 	// 	close(ClientSocket); exit(-1);
 	// }
 	close(ClientSocket);
+}
+
+// //check if the ip address in correct form
+// bool check_ip(char *ip) {
+//     char tmp[16];
+//     int sections = 0;
+//     char *ip_length[16];
+
+// 	memset(tmp, '\0', 16);
+// 	strcpy(tmp, ip);
+	
+// 	ip_length[sections] = strtok(tmp, ".");
+// 	while (ip_length[sections] != NULL) { 
+// 		ip_length[++sections] = strtok(NULL, "."); 
+// 	}
+    
+//     for (int i = 0; i < sections; ++i) { 
+// 			for (int j = 0; j < strlen(ip_length[i]); ++j) { 
+// 				if (ip_length[i][j] < '0' || ip_length[i][j] > '9')	return 0;
+// 			}
+// 			int check = atoi(ip_length[i]); 
+//             if(check < 0 || check > 256) return 0;
+// 		}
+// 	if (sections != 4)return 0; 
+// 	return 1;
+// }
+
+// //Check if the port in the correct form
+bool check_port(char *port_num){
+    if(strlen(port_num) != 4) return 0;
+    else{
+        for(int i = 0; i < strlen(port_num); i++){
+            if(!isdigit(port_num[i])){
+                perror("Invalid Port Number!");
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+// print AUTHOR information
+void AUTHOR(){
+	// Print the author information
+	cse4589_print_and_log("[AUTHOR:SUCCESS]\n");
+	cse4589_print_and_log("I, %s, have read and understood the course academic integrity policy.\n", "haifengx");
+	cse4589_print_and_log(("I, %s, have read and understood the course academic integrity policy.\n", "wzhou29"));
+	cse4589_print_and_log("[AUTHOR:END\n]");
+}
+void IP(){
+	char* ip;
+	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	inet_pton(AF_INET, "8.8.8.8", &addr.sin_addr);  // set ip address to google's ip address
+	addr.sin_port = htons(53);
+
+	// connecting socket
+	if(connect(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+		cse4589_print_and_log("[%s:ERROR]\n", "IP");
+		cse4589_print_and_log("[%s:END]\n", "IP");
+		return;
+	}
+	bzero(&addr, sizeof(addr));
+	int addr_len = sizeof(addr);
+	getsockname(sockfd, (struct sockaddr *)&addr, &addr_len);
+	inet_ntop(AF_INET, &addr.sin_addr, ip, sizeof(ip));
+
+	cse4589_print_and_log("[%s:SUCCESS]\n", "IP");
+	cse4589_print_and_log("IP:%s\n", ip);
+	cse4589_print_and_log("[%s:END]\n", "IP");
+	close(sockfd);
+	return; 
+}
+
+// Print Port information
+void PORT(){
+	struct user curr_user;
+	cse4589_print_and_log("[PORT:SUCCESS]\n");
+	cse4589_print_and_log("PORT:%d\n", curr_user.port_num);
+	cse4589_print_and_log("[PORT:END]\n");
+}
+
+void LIST(){
+	struct user curr_user;
+	int numOf_user;
+	struct user *list;
+	char **ret;
+
+	for(int i = 0; i < numOf_user; i++) {
+		int j = i;
+		for(int k = i+1; k < numOf_user; ++j) {
+			if(list[k].port_num < list[j].port_num) {
+				j = k;
+			}
+		}
+		struct user tmp_user = list[i];
+		list[i] = list[j];
+		list[j] = tmp_user;
+
+	}
+	cse4589_print_and_log("[LIST:SUCCESS]\n]");
+	int count = 1;
+	for(int i = 0; i < numOf_user; ++i) {
+		if(curr_user.is_logged_in) {
+			char *buf = (char*) malloc(sizeof(char)*500);
+			bzero(buf, 500);
+			sprintf(buf, "%-5d%-35s%20s%-8d\n", count, list[i].hostname, list[i].ip_addr, list[i].port_num);
+			ret[count-1] = buf;
+			count++;
+		}
+	}
+	cse4589_print_and_log("[LIST:END]\n]");
+
+}
+
+void STATISTICS(){
+}
+
+void BLOCKED(){
+}
+
+void LOGIN(){
+}
+
+void REFRESH(){
+}
+
+void SEND(){
+}
+
+void BROADCAST(){
+}
+
+void BLOCK(){
+}
+
+void UNBLOCK(){
+}
+
+void LOGOUT(){
+}
+
+void EXIT(){
+}
+
+void SENDFILE(){
+	cse4589_print_and_log("[%s:ERROR]\n", "IP");
+	cse4589_print_and_log("[%s:END]\n", "IP");
 }
