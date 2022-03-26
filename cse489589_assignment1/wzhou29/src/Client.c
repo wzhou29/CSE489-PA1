13fe985e32bf82836ab2e4417af0e4f8770073e2
HaifengXiao-29 <70769700+HaifengXiao-29@users.noreply.github.com>
2022-03-25 21:59:05 -0400

    Update Client.c

--- cse489589_assignment1/wzhou29/src/Client.c
+++ cse489589_assignment1/wzhou29/src/Client.c
@@ -1,6 +1,6 @@
 #include "../include/setting.h"
 
-void ClientHost(int PortNumber) {  
+void ClientHost(int PortNumber) { 
 	int server = -1;
 	char *list_storage = (char*) malloc(sizeof(char)*1000);
 	bzero(list_storage,1000);
@@ -14,26 +14,21 @@ void ClientHost(int PortNumber) {
 	
 	while(1) { 		
 		memcpy(&watch_list, &master_list, sizeof(master_list)); 
-		if ((selret = select(head_socket + 1, &watch_list, NULL, NULL, NULL)) < 0) { 
-			perror("select() Error"); 
-			exit(-1);
-		} 
+		if ((selret = select(head_socket + 1, &watch_list, NULL, NULL, NULL)) < 0) { exit(-1); } 
 		else {
 			for (sock_idx = 0; sock_idx <= head_socket; ++sock_idx) { 
 				if (FD_ISSET(sock_idx, &watch_list)) { 
 					if (sock_idx == STDIN) {
-						char *input = (char*) malloc(sizeof(char)*MsgSize);
+						char *input = (char*) calloc(MsgSize, sizeof(char));
 						bzero(input,CMDSize);
-						if(fgets(input, MsgSize-1, stdin) == NULL) {
-							exit(-1);
-						}
+						if(fgets(input, MsgSize-1, stdin) == NULL) {exit(-1); }
 						fix(input);
-						char *command = (char*) malloc(sizeof(char)*CMDSize);
+						char *command = (char*) calloc(CMDSize, sizeof(char));
 						bzero(command,CMDSize);
 						int num_args = 0; 
 						char *args[100];
 						if (strcmp("", input) != 0) { 
-							char *temp = (char*) malloc(sizeof(char)*strlen(input)); 
+							char *temp = (char*) calloc(strlen(input), sizeof(char)); 
 							strcpy(temp, input); 
 							args[num_args] = strtok(temp, " "); 
 							while (args[num_args] != NULL) { 
@@ -41,9 +36,7 @@ void ClientHost(int PortNumber) {
 							}
 							strcpy(command, args[0]); 
 							fix(command); 
-							if (num_args == 1) { 
-								args[0][strlen(args[0])] = '\0';
-							}
+							if (num_args == 1) { args[0][strlen(args[0])] = '\0'; }
 						}						 
 						if (strcmp(command, "AUTHOR") == 0) { AUTHOR();}
 						else if (strcmp(command, "IP") == 0) { IP();; }
@@ -53,45 +46,8 @@ void ClientHost(int PortNumber) {
 							cse4589_print_and_log("Didin't login to Server\n");
 							cse4589_print_and_log("[%s:END]\n", command);
 						}
-						else if (strcmp(command, "LOGIN") == 0) {
-							if (num_args == 3) { 
-								server = ConnectToServer(args[1], args[2], PortNumber); 
-								if (server > -1) { 
-									FD_SET(server, &master_list); 
-									if (server > head_socket) { 
-										head_socket = server; 
-									} 
-									login = 1; 
-								} 
-								else { 
-									cse4589_print_and_log("[%s:ERROR]\n", command);
-									cse4589_print_and_log("[%s:END]\n", command);
-								}
-							}
-						}
-						else if (strcmp(command, "EXIT") == 0) {
-							if (server == -8) {
-								server = ConnectToServer(args[1], args[2], PortNumber); 
-							}
-							if (server > 0) { 
-								send(server, command, strlen(command), 0); 
-							}
-							cse4589_print_and_log("[%s:SUCCESS]\n", command);
-							cse4589_print_and_log("[%s:END]\n", command);
-							exit(0); 
-						}
-						else {
-							if (server > 0) { 
-								if (strcmp("LIST", command) == 0) {
-									cse4589_print_and_log("[%s:SUCCESS]\n", command);
-									cse4589_print_and_log("%s", list_storage); 
-									cse4589_print_and_log("[%s:END]\n", command);
-								}		
-							}
-						}
-					}
-				}
+
 			}
 		}
 	}
-}
+}}}
